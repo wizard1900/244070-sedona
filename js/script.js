913231f42863eb1eea90c989c12c81274a8005fb
Igor Suchkov <ilgas@yandex.ru>
2017-02-12 18:03:54 +0300

    Финальная верстка

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var link = document.querySelector(".find-hotel");
+      var popup = document.querySelector(".modal-hotel-form");
+       link.addEventListener("click", function(event) {
+        event.preventDefault();
+        popup.classList.toggle("modal-hotel-form-hidden");
+      });
